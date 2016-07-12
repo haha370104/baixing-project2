a5637bc183ddd482cf50e76f6a3cae2e95c850c0
haha370104 <137368440@qq.com>
2016-07-12 10:27:18 +0800

    适配服务器

--- controller/wechat.py
+++ controller/wechat.py
@@ -1,15 +1,18 @@
 from flask import Blueprint, render_template, request
 from tools.security import *
+import xmltodict
 
 wechat_bp = Blueprint('wechat', __name__)
 
 
 @wechat_bp.route('/wechat_server/', methods=['GET', 'POST'])
 def wechat_server():
-    print(request.url)
     if check_signature('token', request.args.get('timestamp'), request.args.get('nonce'),
                        request.args.get('signature')):
-        print(request.values.get('FromUserName'))
-        return 'success'
+        if 'ehcostr' in request.args:
+            return request.args.get('ehcostr')
+        else:
+            print(request.data)
+            return 'success'
     else:
         return request.url
