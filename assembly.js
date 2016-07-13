cab51d67060b9361515a43d4162378acb04fa106
wadechen <wadechen@outlook.com>
2016-07-13 15:37:23 +0800

    add test case .json

---
+++ assembly.js
@@ -0,0 +1,17 @@
+{
+  "success": true,
+  "meeting_rec": [
+    {
+      "id": 100000,
+      "meeting_creater": "lin",
+      "meeting_name": "今日daily",
+      "meeting_time": "2016-10-10"
+    },
+    {
+      "id": 10000,
+      "meeting_creater": "chen",
+      "meeting_name": "yesterday",
+      "meeting_time": "2016-11-25"
+    }
+  ]
+}
